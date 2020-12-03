2bdd8689c23bc6ddbfc79d8981b65d2ca2fdc610
Tom Rawcliffe <tom.rawcliffe@gmail.com>
2020-12-03 17:05:30 +0000

    Part2 for day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,70 +1,73 @@
+use itertools::Itertools;
 use std::io::{self, Read};
-
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
     let input: Vec<u32> = buffer.lines()
-    							.into_iter()
-    							.filter_map(|x| u32::from_str_radix(x, 10).ok())
-    							.collect();
-   	let output = do_stuff(input);
+                                .into_iter()
+                                .filter_map(|x| u32::from_str_radix(x, 10).ok())
+                                .collect();
+    let output = do_stuff(input, 3, 2020);
 
-   	match output {
-		Some(x) => println!("Found value: {}", x),
-		None => println!("Not Found")
-   	}
+    match output {
+        Some(x) => println!("Found value: {}", x),
+        None => println!("Not Found")
+    }
 
     Ok(())
 }
 
 
-pub fn do_stuff(input: Vec<u32>) -> Option<u32> {
-	let mut list = input.clone();
-	list.sort_unstable();
-
-	for i in 0..list.len() {
-		let low = list[i];
-		for high in list[i..].iter().rev() {
-			let sum = high + low;
-
-			if sum == 2020 {
-				return Some(high * low);
-			}
-		}
-	}
-
-	None
+pub fn do_stuff(input: Vec<u32>, num_values: usize, find: u32) -> Option<u32> {
+    Some(input.iter().combinations(num_values)
+        .find(|x| x.iter().fold(0, |acc, j| acc + *j) == find)?
+        .iter()
+        .fold(1, |acc, v| acc * *v))
 }
 
 
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
     #[test]
-    fn does_stuff() {
+    fn part1() {
         assert_eq!(do_stuff(vec![
-        	1721,
-			979,
-			366,
-			299,
-			116,
-			2000,
-			675,
-			1456,
-        ]), Some(514579));
+            1721,
+            979,
+            366,
+            299,
+            116,
+            2000,
+            675,
+            1456,
+        ], 2, 2020), Some(514579));
 
         assert_eq!(do_stuff(vec![
-        	1721,
-			979,
-			366,
-			300,
-			116,
-			2000,
-			675,
-			1456,
-        ]).is_none(), true)
+            1721,
+            979,
+            366,
+            300,
+            116,
+            2000,
+            675,
+            1456,
+        ], 2, 2020).is_none(), true)
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(do_stuff(vec![
+            1721,
+            979,
+            366,
+            299,
+            116,
+            2000,
+            675,
+            1456,
+        ], 3, 2020), Some(241861950));
     }
 }
