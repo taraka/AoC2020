b287ae6e1092a690425c88d80132030075b3d543
Tom Rawcliffe <tom.rawcliffe@gmail.com>
2020-12-03 10:32:41 +0000

    Shorter day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,16 +4,23 @@ fn main() -> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
-    let n = num_trees(&buffer);
+    let n = check_slopes(&buffer, vec![(1,1), (3,1), (5,1), (7,1), (1,2)]);
     println!("Found {} trees", n);
 
     Ok(())
 }
 
-fn num_trees(forest: &str) -> u32 {
-	forest.lines().enumerate().filter(|(y, row)| 
-		row.chars().nth((y * 3) % row.chars().count()).unwrap() == '#' 
-	).count() as u32
+fn check_slopes(forest: &str, slopes: Vec<(usize, usize)>) -> u64 {
+	slopes.iter().map(|(x, y)| num_trees(forest, *x, *y))
+		.fold(1, |acc, v| acc * v)
+}
+
+fn num_trees(forest: &str, x_step: usize, y_step: usize) -> u64 {
+	forest.lines().enumerate()
+	.filter(|(y,_)| y % y_step == 0 )
+	.filter(|(y, row)| 
+		row.chars().nth(((y/y_step) * x_step) % row.chars().count()).unwrap() == '#' 
+	).count() as u64
 }
 
 #[cfg(test)]
@@ -34,6 +41,8 @@ mod tests {
 #...##....#
 .#..#...#.#"#;
 
-        assert_eq!(num_trees(input), 7);
+        assert_eq!(num_trees(input, 3, 1), 7);
+        assert_eq!(check_slopes(input, vec![(1,1), (3,1), (5,1), (7,1), (1,2)]), 336);
+
     }
 }
