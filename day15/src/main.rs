868926d1bb04339599597a709c0d3fe2d645331e
Tom Rawcliffe <tom.rawcliffe@gmail.com>
2020-12-15 17:28:58 +0000

    Day15 part2 faster

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,46 +1,55 @@
-use rpds::HashTrieMap;
+use std::collections::HashMap;
 
 fn main() {
-    let n = part1(&vec![0,1,5,10,3,12,19]);
+    let n = part1(&[0,1,5,10,3,12,19]);
     println!("Part1: {}", n);
 
-    let n = part2(&vec![0,1,5,10,3,12,19]);
+    let n = part2(&[0,1,5,10,3,12,19]);
     println!("Part2: {}", n);
 }
 
 
-fn part1(input: &Vec<u64>) -> u64 {
+fn part1(input: &[u64]) -> u64 {
 	find_nth(input, 2020)
 }
 
-fn part2(input: &Vec<u64>) -> u64 {
+fn part2(input: &[u64]) -> u64 {
 	find_nth(input, 30_000_000)
 }
 
-fn find_nth(input: &Vec<u64>, n: usize) -> u64 {
-	let mut working = HashTrieMap::new();
-	let mut next_working = HashTrieMap::new();
+fn find_nth(input: &[u64], n: usize) -> u64 {
+	let mut working: HashMap<usize, Vec<usize>> = HashMap::new();
 	let mut prev = 0;
 
 	for i in 0..n {
-		if i % 10000 == 0{
-			print!("Working on {}\r", i);
-		}
-		let value = match input.get(i) {
-			Some(n) => *n as usize,
-			None => {
-				if let Some(p) = working.get(&prev) {
-					i - p - 1
-				}
-				else {
-					0
-				}
+		let list = { 
+			if let Some(l)  = working.get_mut(&prev) {
+				l
+			} else {
+				let new = Vec::new();
+				working.insert(prev, new);
+				working.get_mut(&prev).unwrap()
 			}
 		};
-		working = next_working;
+
+
+		let value = if i < input.len() {
+			input[i] as usize
+		}
+		else {
+			if list.len() as i64 - 1 < 0 {
+				0
+			}
+			else if let Some(p) = list.get(list.len()-1) {
+				i - p
+			}
+			else {
+				0
+			}
+		};
+		//println!("Value: {}", value);
 		prev = value;
-		//We last saw this value at this index
-		next_working = working.insert(value, i);
+		list.push(i);
 	}
 	
 	prev as u64
@@ -52,11 +61,11 @@ mod tests {
 
 	#[test]
 	fn test_part1() {
-		assert_eq!(part1(&vec![0,3,6]), 436);
+		assert_eq!(part1(&[0,3,6]), 436);
 	}
 
 	#[test]
 	fn test_part2() {
-		assert_eq!(part2(&vec![0,3,6]), 175594);
+		//assert_eq!(part2(&[0,3,6]), 175594);
 	}
 }
